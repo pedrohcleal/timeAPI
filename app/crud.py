ca64e2d62f8e776bbb1327af36bf24c755cd35a8
Pedro Leal <59542610+pedrohcleal@users.noreply.github.com>
2024-11-10 15:51:26 -0300

    fix def

--- app/crud.py
+++ app/crud.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+
 def get_only_countries(conn) -> list[str]:
     try:
         countries = conn.execute("SELECT * FROM cities_and_countries").fetchall()
@@ -9,9 +10,12 @@ def get_only_countries(conn) -> list[str]:
         print(f"SQL error = {e}")
         return []
 
+
 def get_all_countries(conn) -> list[str]:
     try:
-        countries = conn.execute("SELECT DISTINCT country FROM cities_and_countries").fetchall()
+        countries = conn.execute(
+            "SELECT DISTINCT country FROM cities_and_countries"
+        ).fetchall()
         countries = [x[0] for x in countries]
         return countries
     except sqlite3.Error as e:
@@ -37,5 +41,5 @@ def insert_into_table(conn: sqlite3.Connection, city: str, country: str) -> bool
         conn.commit()
         return True
     except sqlite3.Error as e:
-        print(f"Error inserting value into {table}: {e}")
+        print(f"Error inserting value, {e}")
         return False
